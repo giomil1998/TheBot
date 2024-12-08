19ac1157fa7192d417637fb23824b74d4f218235
giomil1998 <Giorgio.Militello@warwick.ac.uk>
2024-12-08 16:00:20 +0000

    Quarterly portfolios latest

--- src/piotroski.py
+++ src/piotroski.py
@@ -3,14 +3,14 @@ from Plotter import Plotter
 from StrategyRunner import StrategyRunner
 
 if __name__ == "__main__":
-    START_DATE = '2000-01-01'
+    START_DATE = '2019-01-01'
     END_DATE = '2024-11-01'
     LONG_PORTFOLIO_SIZE = 20  # Maximum number of positions in long portfolio
     SHORT_PORTFOLIO_SIZE = 10  # Maximum number of positions in short portfolio
     INACTIVITY_THRESHOLD = 30  # Number of consecutive days below market cap threshold
     MARKET_CAP_THRESHOLD = 2_000_000_000  # Minimum market cap in dollars
     PORTFOLIO_UPDATE_DELAY = 60  # Number of days before the data in a report is used to update portfolios
-    GET_NEW_DATA = True
+    GET_NEW_DATA = False
 
     fundq, crsp = DataHandler.fetch_or_read_data(GET_NEW_DATA, START_DATE, END_DATE)
 
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     strategy_runner.save_portfolios_to_csv()
     cumulative_strategy_returns = strategy_runner.process_returns()
 
-    Plotter.plot_strategy_returns(cumulative_strategy_returns, fundq["datadate"].min())
+    Plotter.plot_strategy_returns(cumulative_strategy_returns)
     print("done")
